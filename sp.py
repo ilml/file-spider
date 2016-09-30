2a05fb4424400820a217e91edc24585173b8c40f
tom long <mytomlong@gmail.com>
2016-09-30 11:29:21 +0800

    Add files via upload

--- sp.py
+++ sp.py
@@ -4,22 +4,28 @@ Created on Tue Sep 27 11:11:10 2016
 
 @author: lty765
 """
-
+'''
+need to add:
+tranform to a single list
+searching of a certain type of file 
+'''
 #Seek all files in a certain directory
 #nta:file name
 def fileseek(addr):
     import os
-    os.chdir(addr)
-    temp = []
-    for file in os.listdir(addr):
-      if file!='$RECYCLE.BIN' and file!='System Volume Information' and file!='Config.Msi':
-        if os.path.isdir(file):
-            newfile = os.path.join(os.getcwd(),file)
-            temp.append(fileseek(newfile))
-            os.chdir('..')
-        else:          
-            temp.append(file)
-    return temp
+    try:
+        os.chdir(addr) 
+        temp = []
+        for file in os.listdir(addr):
+            if os.path.isdir(file):
+                newfile = os.path.join(os.getcwd(),file)
+                temp.append(fileseek(newfile))
+                os.chdir('..')
+            else:          
+                temp.append(file)
+        return temp
+    except:
+        return  []
     
 #print(fileseek('H:\\python'))
 
@@ -38,20 +44,28 @@ def disk():
 #print(disk())
 
 #nta:disk name
+#currently not use,spending too much time!
 def diskseek():
     hard_disk = disk()
     temp = []
     for item in hard_disk:
-        temp.append(fileseek(item))
+        if item != 'C:':
+            temp.append(fileseek(item+'\\'))
     return temp
-  
-def test():
-    import time
-    start = time.time()
-    fileseek('G:\\')
-    end = time.time()
-    cost = end - start
-    print(cost)
+
+
+#done 
+def test(choose_disk='H:'):
+    disk_now = disk()
+    if choose_disk in disk_now:        
+        import time
+        start = time.time()
+        fileseek(choose_disk+'\\')
+        end = time.time()
+        cost = end - start
+        print(cost)
+    else:
+        print('Disk doesn\'t exist,please try another disk.')
     
     
     
